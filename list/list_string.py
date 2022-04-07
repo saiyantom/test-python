f84a04ab346bf44e75e7ddd962555950e621c6e0
tom <tom160377@gmail.com>
2022-04-07 11:19:03 +0000

    20220407

---
+++ list/list_string.py
@@ -0,0 +1,6 @@
+st1 = ["you","are","wonderful","guy"]
+
+for i in range(len(st1)):
+    st1[i]=st1[i].upper()
+
+print(st1)
---
+++ list/list_string2.py
@@ -0,0 +1,8 @@
+st1 = ["you","are","the","best"]
+st2 = []
+
+for i in range(len(st1)):
+    st2.append(st1[i].capitalize()[0])
+
+print(st1)
+print(st2)
