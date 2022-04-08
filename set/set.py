ed961b06ac6e093718649e8570d667286cc4422a
tom <tom160377@gmail.com>
2022-04-08 07:42:28 +0000

    set

--- set/set.py
+++ set/set.py
@@ -1,27 +1,21 @@
 import random
 
 s=set()
-c=b=0
+gt_set=set()
+c=0
 
 while len(s)<10:
-    #for k in range (10):
     n = random.randint(15,46)
     s.add(n)
 
 print(f'Before operation: {s}')
 
-#my_list=list(s)
-#print(f'After conversion: {my_list}\n')
+for item in s:
+    if item > 35:
+        gt_set.add(item)
+    elif item < 30:
+        c+=1
 
-#for i in range(len(my_list)-1):
-#    if my_list[i] < 30:
-#        c+=1
-#        continue
-#    if my_list[i] > 35:
-#        b+=1
-#        my_list.remove(my_list[i])
-#        print(f'list after removal: {my_list}')
- 
-#print(f'After operation: {my_list}')
-#print(f'Number of elements<30: {c}')
-#print(f'Number of elements>35: {b}')
+print(f'No. of elements < 30: {c}')
+print(f'No. of elements > 35: {gt_set}')
+print(f'After operation: {s-gt_set}')
